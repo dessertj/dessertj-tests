f09a1ba3eb71c13f194758915738085975d3fbdc
hajo70 <hessmann@spricom.de>
2021-11-05 22:06:45 +0100

    Dump content of module classes

---
+++ dessert-tests-modules-parent/dessert-tests-modules-app/src/test/java/de/spricom/dessert/samples/modules/app/ModuleClassfileTest.java
@@ -0,0 +1,29 @@
+package de.spricom.dessert.samples.modules.app;
+
+import de.spricom.dessert.classfile.ClassFile;
+import de.spricom.dessert.samples.modules.lib.Greeter;
+import de.spricom.dessert.samples.modules.lib2.magic.MagicService;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class ModuleClassfileTest {
+
+    @Test
+    public void dumpAppModule() throws IOException {
+        ClassFile cf = new ClassFile(App.class.getResourceAsStream("/module-info.class"));
+        System.out.println(cf.dump());
+    }
+
+    @Test
+    public void dumpLibModule() throws IOException {
+        ClassFile cf = new ClassFile(Greeter.class.getResourceAsStream("/module-info.class"));
+        System.out.println(cf.dump());
+    }
+
+    @Test
+    public void dumpLib2Module() throws IOException {
+        ClassFile cf = new ClassFile(MagicService.class.getResourceAsStream("/module-info.class"));
+        System.out.println(cf.dump());
+    }
+}
